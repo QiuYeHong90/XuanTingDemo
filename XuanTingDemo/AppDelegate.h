2fb872f458e626a8e6e486ebf522aeab32b1ca84
小灰灰 <793983383@qq.com>
2018-05-01 15:53:20 +0800

    Initial Commit

---
+++ XuanTingDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XuanTingDemo
+//
+//  Created by Mac on 2018/5/1.
+//  Copyright © 2018年 袁书辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
